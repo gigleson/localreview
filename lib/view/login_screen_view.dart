74b6e4334d5d5530ae46e1a3e34d91981741af3f
gigleson <kaushalranjit11@gmail.com>
2024-12-20 19:27:09 +0545

    changed degine login

--- lib/view/login_screen_view.dart
+++ lib/view/login_screen_view.dart
@@ -1,194 +1,219 @@
 import 'package:flutter/material.dart';
-import 'package:localreview/view/home_screen_view.dart';
+import 'package:localreview/core/themes/mythemes.dart';
+
+import 'package:localreview/view/navbar_view.dart';
 import 'package:localreview/view/signup_screen_view.dart';
 
 class LoginScreenView extends StatefulWidget {
-  const LoginScreenView({Key? key}) : super(key: key);
+  const LoginScreenView({super.key});
 
   @override
   State<LoginScreenView> createState() => _LoginScreenViewState();
 }
 
 class _LoginScreenViewState extends State<LoginScreenView> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
-  void login() {
-    final email = emailController.text.trim();
-    final password = passwordController.text.trim();
-
-    if (_formKey.currentState!.validate()) {
-      if (email == 'admin@gmail.com' && password == 'admin') {
-        // Navigate to the Home Page
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const HomeScreenView()),
-        );
-      } else {
-        // Show error snackbar
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text('Invalid email or password.'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppTheme.primary,
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF0072FF), Color(0xFF00C6FF)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    // App Logo
-                    Image.asset(
-                      'assets/images/logo.png',
-                      height: 100,
-                      width: 100,
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
+        color: AppTheme.tertiary,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            // Top Image Section
+            Container(
+              width: double.infinity,
+              height: 200, // Adjust height as needed
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/onboarding3.jpg'),
+                ),
+              ),
+            ),
+            // Bottom Content Section
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppTheme.secondaryBackground,
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 7,
+                      color: Color(0x4D090F13),
+                      offset: Offset(0, 3),
                     ),
-                    const SizedBox(height: 30),
-
-                    // Welcome Text
-                    const Text(
-                      'Hi There!',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'Log in to continue',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white70,
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-
-                    // Email TextField
-                    TextFormField(
-                      controller: emailController,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        hintStyle: const TextStyle(color: Colors.white70),
-                        filled: true,
-                        fillColor: Colors.white24,
-                        prefixIcon:
-                            const Icon(Icons.email, color: Colors.white),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your email';
-                        }
-                        if (!RegExp(
-                                r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
-                            .hasMatch(value)) {
-                          return 'Please enter a valid email address';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-
-                    // Password TextField
-                    TextFormField(
-                      controller: passwordController,
-                      style: const TextStyle(color: Colors.white),
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        hintStyle: const TextStyle(color: Colors.white70),
-                        filled: true,
-                        fillColor: Colors.white24,
-                        prefixIcon: const Icon(Icons.lock, color: Colors.white),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-
-                    // Login Button
-                    ElevatedButton(
-                      onPressed: login,
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        backgroundColor: Colors.blue.shade700,
-                      ),
-                      child: const Text(
-                        'Log In',
-                        style: TextStyle(fontSize: 18, color: Colors.black),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-
-                    // Don't have an account? Sign Up
-                    Row(
+                  ],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        const Text(
-                          'Don’t have an account?',
-                          style: TextStyle(color: Colors.white70),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 56),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/images/logo.png',
+                                width: 150,
+                                height: 150,
+                              ),
+                            ],
+                          ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            // Navigate to Sign-Up Page
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const SignupScreenView()),
-                            );
-                          },
-                          child: const Text(
-                            'Sign Up',
-                            style: TextStyle(color: Colors.white),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24, bottom: 4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Welcome Back,',
+                                style: AppTheme.displaySmall,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Email Address',
+                                    hintText: 'Enter your email here...',
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: AppTheme.lineGray,
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    filled: true,
+                                    fillColor: AppTheme.secondaryBackground,
+                                    contentPadding: const EdgeInsets.fromLTRB(
+                                        16, 24, 0, 24),
+                                  ),
+                                  style: AppTheme.titleSmall,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextFormField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    labelText: 'Password',
+                                    hintText: 'Enter your password here...',
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: AppTheme.lineGray,
+                                        width: 2,
+                                      ),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    filled: true,
+                                    fillColor: AppTheme.secondaryBackground,
+                                    contentPadding: const EdgeInsets.fromLTRB(
+                                        16, 24, 24, 24),
+                                  ),
+                                  style: AppTheme.titleSmall,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Forgot Password?',
+                                  style: AppTheme.titleSmall,
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const BottomNavationView()
+                                        // const ScreenView(),
+                                        ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: AppTheme.primary,
+                                  elevation: 3,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(40),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Login',
+                                  style: AppTheme.titleMedium,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12, bottom: 24),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Don\'t have an account?',
+                                style: AppTheme.bodyMedium,
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const SignupScreenView()),
+                                  );
+                                },
+                                child: Text(
+                                  'Create Account',
+                                  style: AppTheme.titleSmall.copyWith(
+                                    color: const Color(0xFF39D2C0),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
