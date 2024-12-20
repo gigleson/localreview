ac44e859c1797c7f12ba936eb19eef8598bd1b91
gigleson <kaushalranjit11@gmail.com>
2024-12-20 21:23:52 +0545

    added navbar

---
+++ lib/core/common/navbar.dart
@@ -0,0 +1,54 @@
+// import 'package:flutter/material.dart';
+// import 'package:localreview/view/feed_screen_view.dart';
+// import 'package:localreview/view/home_screen_view.dart';
+// import 'package:localreview/view/profile_view.dart';
+
+// class BottomNavationView extends StatefulWidget {
+//   const BottomNavationView({super.key});
+
+//   @override
+//   State<BottomNavationView> createState() => _BottomNavationViewState();
+// }
+
+// class _BottomNavationViewState extends State<BottomNavationView> {
+//   int _index = 0;
+//   List<Widget> screens = [
+//     const HomeScreenView(),
+//     const FeedScreenView(),
+//     const ProfileView(),
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("bottom navation"),
+//       ),
+//       body: screens[_index],
+//       bottomNavigationBar: BottomNavigationBar(
+//         backgroundColor: Colors.green,
+//         currentIndex: _index,
+//         onTap: (index) {
+//           setState(() {
+//             _index = index;
+//           });
+//         },
+//         type: BottomNavigationBarType.fixed,
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Feed',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: 'Profile',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.explore),
+//             label: 'Explore',
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
