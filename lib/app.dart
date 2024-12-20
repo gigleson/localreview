8135963a400ae3c11c17f58f3b0d95f79f0d1c88
gigleson <kaushalranjit11@gmail.com>
2024-12-20 21:22:41 +0545

    added fonts

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:localreview/view/splash_screen_view.dart';
 
-
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Ubantu Normal",
+      ),
       title: "Local Review",
-      home: SplashScreenView(),
+      home: const SplashScreenView(),
     );
   }
 }
