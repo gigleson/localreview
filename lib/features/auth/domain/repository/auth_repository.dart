6546d3b3255344dd43db83ffc0afdf17276b600a
gigleson <kaushalranjit11@gmail.com>
2025-01-19 07:35:06 +0545

    added domain layer

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:localreview/core/error/failure.dart';
+import 'package:localreview/features/auth/domain/entity/auth_entity.dart';
+
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure,String>> loginUser(String username, String password);  
+  Future<Either<Failure,void>> registerUser(AuthEntity user);  
+  Future<Either<Failure,AuthEntity>> getCurrentUser();  
+  
+}
